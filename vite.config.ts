e7fce34607eb1368b2064a1865dbef9ffc34b943
SamiBouchareb <Boucharebismail@gmail.com>
2024-11-29 21:10:59 +0100

    Add Netlify configuration and update Vite config for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Opa/',
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
